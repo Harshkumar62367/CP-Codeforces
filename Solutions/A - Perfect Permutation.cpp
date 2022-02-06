11eb35dc1fbbe8441c84468ccea3d05e0bf72851
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-02-07 01:36:28 +0530

    Create A - Perfect Permutation.cpp

---
+++ Solutions/A - Perfect Permutation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n%2==0)
+	{
+	    for(int i=n; i>0; i--)
+	    {
+	        cout<<i<<" ";
+	    }
+	}
+	else
+	{
+	    cout<<-1;
+	}
+	return 0;
+}
