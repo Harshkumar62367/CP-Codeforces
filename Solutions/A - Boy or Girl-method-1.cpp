0628fad906c4f5b3c52c11085ab03cc4eb08f2f4
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-02-13 01:17:41 +0530

    Create A - Boy or Girl-method-1.cpp

---
+++ Solutions/A - Boy or Girl-method-1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string s;
+	cin>>s;
+	int count=0;
+	int a[123]={0};
+	for(int i=0; i<s.length();i++)
+	{
+	    int x=s[i];
+	    a[x]++;
+	}
+	int len=s.length();
+	for(int i=95; i<123; i++)
+	{
+	    if(a[i]>=1)
+	    {
+	        len=len-a[i]+1;
+	    }
+	}
+	if(len%2==0)
+	{
+	    cout<<"CHAT WITH HER!";
+	}
+	else
+	{
+	    cout<<"IGNORE HIM!";
+	}
+	return 0;
+}
