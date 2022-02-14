d011b6749f85404a020905ba5707face5bc9efbb
Harsh Kumar <72465090+Harshkumar62367@users.noreply.github.com>
2022-02-14 22:36:49 +0530

    Create A - Watermelon.cpp

---
+++ Solutions/A - Watermelon.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n==2)
+	{
+	    cout<<"NO";
+	}
+	else if(n%2==0)
+	{
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO";
+	}
+	return 0;
+}
